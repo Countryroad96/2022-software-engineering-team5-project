1709fbc2357d66bb5a30833e03eb36c4d5d019ba
JangWonSeok <wkddnjstjr74@naver.com>
2022-06-11 02:42:57 +0900

    회원가입 오류 예외처리 추가 및 성공 알림 추가
    +사양에 맞게 입력 form 수정 (register.html)

--- app.py
+++ app.py
@@ -21,20 +21,23 @@ def mainpage():
 
 # GET(정보보기), POST(정보수정) 메서드 허용
 @app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])    # GET(정보보기), POST(정보수정) 메서드 허용
 def register():
     form = RegisterForm()
     if form.validate_on_submit():  # 유효성 검사. 내용 채우지 않은 항목이 있는지까지 체크
-        usertable = User(form.data.get('userid'),
-                         form.data.get('email'),
-                         form.data.get('password'))
-        # usertable.userid = form.data.get('userid')
-        # usertable.email = form.data.get('email')
-        # usertable.password = form.data.get('password')
-
-        db.session.add(usertable)  # DB저장
-        db.session.commit()  # 변동사항 반영
-
-        return "회원가입 성공"
+        message = None
+        user = User.query.filter_by(userid=form.data.get('userid')).first()
+        if user:
+            message = '이미 존재하는 아이디 입니다.'
+            flash(message)
+        else:
+            message = '회원가입 성공'
+            usertable = User(form.data.get('userid'),
+                            form.data.get('password'))
+            db.session.add(usertable)  # DB저장
+            db.session.commit()  # 변동사항 반영
+            flash(message)
+            return redirect('/')
     return render_template('register.html', form=form)  # form이 어떤 form인지 명시한다
 
 
@@ -66,12 +69,10 @@ def logout():
 
 if __name__ == "__main__":
     # 데이터베이스---------
-    basedir = os.path.abspath(os.path.dirname(
-        __file__))  # 현재 파일이 있는 디렉토리 절대 경로
+    basedir = os.path.abspath(os.path.dirname(__file__))  # 현재 파일이 있는 디렉토리 절대 경로
     dbfile = os.path.join(basedir, 'db.sqlite')  # 데이터베이스 파일을 만든다
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    # 사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile   # 사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 추가 메모리를 사용하므로 꺼둔다
     app.config['SECRET_KEY'] = 'secretkey'  # 해시값은 임의로 적음
