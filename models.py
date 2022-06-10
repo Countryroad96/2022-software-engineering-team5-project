d59e89dd631f243d8a253af0507759358e84cd42
JangWonSeok <wkddnjstjr74@naver.com>
2022-06-11 02:38:55 +0900

    User model 사양에 맞게 수정

--- models.py
+++ models.py
@@ -3,22 +3,30 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()  # SQLAlchemy를 사용해 데이터베이스 저장
 
-
 class User(db.Model):  # 데이터 모델을 나타내는 객체 선언
     __tablename__ = 'user_table'  # 테이블 이름
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(32), unique=True, nullable=False)
     userid = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(8), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    following_list = db.Column(db.String(400))
 
-    def __init__(self, userid, email, password, **kwargs):
+    def __init__(self, userid, password, **kwargs):
         self.userid = userid
-        self.email = email
         self.set_password(password)
+        self.following_list = ""
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    def set_userid(self, userid):
+        self.userid = userid
+        
+    def add_following(self, userid):
+        self.following_list = list_to_string(self.following_list.split().append(userid))
+    
+    def get_following(self):
+        return self.following_list.split()
